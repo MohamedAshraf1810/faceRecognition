7fdc4c0cbd65f0e0cb0e44d3d21b1475d213548d
Mohamed Ashraf <mohamedashraf3333@hotmail.com>
2022-12-05 00:27:34 +0200

    first commit

---
+++ Dumbed_Model/dumping.py
@@ -0,0 +1,45 @@
+import face_recognition
+from sklearn import svm
+import os
+import cv2
+import pickle
+
+
+# Training the SVC classifier
+
+# The training data would be all the face encodings from all the known images and the labels are their names
+encodings = []
+names = []
+
+# Training directory
+train_dir = os.listdir('train_dir/')
+
+# Loop through each person in the training directory
+for person in train_dir:
+    pix = os.listdir("train_dir/" + person)
+
+    # Loop through each training image for the current person
+    for person_img in pix:
+        print("Checking Train Images ..." , person_img)
+        # Get the face encodings for the face in each image file
+        face = face_recognition.load_image_file("train_dir/" + person + "/" + person_img)
+        face_bounding_boxes = face_recognition.face_locations(face)
+
+        # If training image contains exactly one face
+        if len(face_bounding_boxes) == 1:
+            face_enc = face_recognition.face_encodings(face)[0]
+            # Add face encoding for current image with corresponding label (name) to the training data
+            encodings.append(face_enc)
+            names.append(person)
+        else:
+            print(person + "/" + person_img + " was skipped and can't be used for training")
+
+# Create and train the SVC classifier
+clf = svm.SVC(gamma='scale')
+clf = svm.SVC(probability=True)
+clf.fit(encodings,names)
+
+with open ('Dumbed_Model/Trained_Model/MAIN/OtherOnly.pk','wb') as f:
+    pickle.dump(clf,f)
+print("trainning Completed")
+
---
+++ face_recognition_svm.py
@@ -0,0 +1,58 @@
+# Train multiple images per person
+# Find and recognize faces in an image using a SVC with scikit-learn
+
+import face_recognition
+from sklearn import svm
+import os
+import cv2
+import pickle
+
+
+
+with open('Dumbed_Model/Trained_Model/MAIN/mohamed_other.pk','rb') as f:
+    mypickle = pickle.load(f)
+
+# Load the test image with unknown faces into a numpy array
+test_image = face_recognition.load_image_file('SVC_Testimgs/TeamPhotos/untitled454545.png')
+
+# Find all the faces in the test image using the default HOG-based model
+face_locations = face_recognition.face_locations(test_image)
+no = len(face_locations)
+print("Number of faces detected: ", no)
+
+
+# Predict all the faces in the test image using the trained classifier
+print("Found:")
+for i in range(no):
+    test_image_enc = face_recognition.face_encodings(test_image)[i]
+    
+    propa_name = mypickle.predict_proba([test_image_enc])
+    
+    # Getting Model Accuracy
+    max_acc = propa_name.max()*100
+    if (max_acc) > 90:
+        name = mypickle.predict([test_image_enc]) 
+        print(*name)
+        print("The Model Accuracy Is = " , round(max_acc,2) , "%")
+    else:
+        name=['UnKnown']
+        print(*name)
+        print("The Model Accuracy Is = " , round(max_acc,2) , "%")
+
+# Print Accuracy
+print(propa_name)
+
+    # Show Result On Screen
+while True:
+    ResImg = cv2.cvtColor(test_image, cv2.COLOR_BGR2RGB)
+    for face in face_locations:
+        top, right, bottom, left = face
+        cv2.rectangle(ResImg, (left, top), (right, bottom), (255, 0, 0), 2)
+        # cv2.rectangle(ResImg, (left, top+109), (right, bottom+30), (0, 255, 255),-1) # make a background for the name
+        cv2.putText(ResImg, *name , (left, bottom + 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        cv2.putText(ResImg, "accuracy : "+ str(round(max_acc,2))+'%' , (left, bottom + 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+    cv2.imshow('img',ResImg)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
